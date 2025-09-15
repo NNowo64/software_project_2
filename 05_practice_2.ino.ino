3ab9088c8f0d2eeee1f8ec2096151fc5a48f0ab1
NNowo64 <entrajangjeon@gmail.com>
2025-09-15 14:46:52 +0900

    Add files via upload

---
+++ 05_practice_2.ino.ino
@@ -0,0 +1,35 @@
+
+void setup() {
+  pinMode(7, OUTPUT);
+
+}
+
+void loop() {
+  
+  digitalWrite(7, 0);
+  delay(1000);
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+
+  digitalWrite(7, 1);
+  while(1) {}
+  
+}
